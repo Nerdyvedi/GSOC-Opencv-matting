370d26fcaa887de251f836db328820a819baaea6
Vedanta Jha <jhavedanta@hotmail.com>
2019-06-22 21:45:05 -0400

    Renamed boxfilter
    renamed it so there is no confusion with boxFilter.
    
    The reason I did not use boxFilter is so that I do not have to initialize a destination Mat every time.

--- Global-Sampling-Method-for-Alpha-Matting/guidedfilter.cpp
+++ Global-Sampling-Method-for-Alpha-Matting/guidedfilter.cpp
@@ -1,6 +1,11 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+#include <precomp.hpp>
 #include "guidedfilter.h"
 
-static cv::Mat boxfilter(const cv::Mat &I, int r)
+//Used to blur the image using the normalized box filter of size r*r
+static cv::Mat custom_blur(const cv::Mat &I, int r)
 {
     cv::Mat result;
     cv::blur(I, result, cv::Size(r, r));
@@ -94,22 +99,22 @@ guidedFilterMono::guidedFilterMono(const cv::Mat &origI, int r, double eps) : r(
 
     Idepth = I.depth();
 
-    mean_I = boxfilter(I, r);
-    cv::Mat mean_II = boxfilter(I.mul(I), r);
+    mean_I = custom_blur(I, r);
+    cv::Mat mean_II = custom_blur(I.mul(I), r);
     var_I = mean_II - mean_I.mul(mean_I);
 }
 
 cv::Mat guidedFilterMono::filterSingleChannel(const cv::Mat &p) const
 {
-    cv::Mat mean_p = boxfilter(p, r);
-    cv::Mat mean_Ip = boxfilter(I.mul(p), r);
+    cv::Mat mean_p = custom_blur(p, r);
+    cv::Mat mean_Ip = custom_blur(I.mul(p), r);
     cv::Mat cov_Ip = mean_Ip - mean_I.mul(mean_p); // this is the covariance of (I, p) in each local patch.
 
     cv::Mat a = cov_Ip / (var_I + eps); // Eqn. (5) in the paper;
     cv::Mat b = mean_p - a.mul(mean_I); // Eqn. (6) in the paper;
 
-    cv::Mat mean_a = boxfilter(a, r);
-    cv::Mat mean_b = boxfilter(b, r);
+    cv::Mat mean_a = custom_blur(a, r);
+    cv::Mat mean_b = custom_blur(b, r);
 
     return mean_a.mul(I) + mean_b;
 }
@@ -126,21 +131,21 @@ guidedFilterColor::guidedFilterColor(const cv::Mat &origI, int r, double eps) : 
 
     cv::split(I, Ichannels);
 
-    mean_I_r = boxfilter(Ichannels[0], r);
-    mean_I_g = boxfilter(Ichannels[1], r);
-    mean_I_b = boxfilter(Ichannels[2], r);
+    mean_I_r = custom_blur(Ichannels[0], r);
+    mean_I_g = custom_blur(Ichannels[1], r);
+    mean_I_b = custom_blur(Ichannels[2], r);
 
     // variance of I in each local patch: the matrix Sigma in Eqn (14).
     // Note the variance in each local patch is a 3x3 symmetric matrix:
     //           rr, rg, rb
     //   Sigma = rg, gg, gb
     //           rb, gb, bb
-    cv::Mat var_I_rr = boxfilter(Ichannels[0].mul(Ichannels[0]), r) - mean_I_r.mul(mean_I_r) + eps;
-    cv::Mat var_I_rg = boxfilter(Ichannels[0].mul(Ichannels[1]), r) - mean_I_r.mul(mean_I_g);
-    cv::Mat var_I_rb = boxfilter(Ichannels[0].mul(Ichannels[2]), r) - mean_I_r.mul(mean_I_b);
-    cv::Mat var_I_gg = boxfilter(Ichannels[1].mul(Ichannels[1]), r) - mean_I_g.mul(mean_I_g) + eps;
-    cv::Mat var_I_gb = boxfilter(Ichannels[1].mul(Ichannels[2]), r) - mean_I_g.mul(mean_I_b);
-    cv::Mat var_I_bb = boxfilter(Ichannels[2].mul(Ichannels[2]), r) - mean_I_b.mul(mean_I_b) + eps;
+    cv::Mat var_I_rr = custom_blur(Ichannels[0].mul(Ichannels[0]), r) - mean_I_r.mul(mean_I_r) + eps;
+    cv::Mat var_I_rg = custom_blur(Ichannels[0].mul(Ichannels[1]), r) - mean_I_r.mul(mean_I_g);
+    cv::Mat var_I_rb = custom_blur(Ichannels[0].mul(Ichannels[2]), r) - mean_I_r.mul(mean_I_b);
+    cv::Mat var_I_gg = custom_blur(Ichannels[1].mul(Ichannels[1]), r) - mean_I_g.mul(mean_I_g) + eps;
+    cv::Mat var_I_gb = custom_blur(Ichannels[1].mul(Ichannels[2]), r) - mean_I_g.mul(mean_I_b);
+    cv::Mat var_I_bb = custom_blur(Ichannels[2].mul(Ichannels[2]), r) - mean_I_b.mul(mean_I_b) + eps;
 
     // Inverse of Sigma + eps * I
     invrr = var_I_gg.mul(var_I_bb) - var_I_gb.mul(var_I_gb);
@@ -162,11 +167,11 @@ guidedFilterColor::guidedFilterColor(const cv::Mat &origI, int r, double eps) : 
 
 cv::Mat guidedFilterColor::filterSingleChannel(const cv::Mat &p) const
 {
-    cv::Mat mean_p = boxfilter(p, r);
+    cv::Mat mean_p = custom_blur(p, r);
 
-    cv::Mat mean_Ip_r = boxfilter(Ichannels[0].mul(p), r);
-    cv::Mat mean_Ip_g = boxfilter(Ichannels[1].mul(p), r);
-    cv::Mat mean_Ip_b = boxfilter(Ichannels[2].mul(p), r);
+    cv::Mat mean_Ip_r = custom_blur(Ichannels[0].mul(p), r);
+    cv::Mat mean_Ip_g = custom_blur(Ichannels[1].mul(p), r);
+    cv::Mat mean_Ip_b = custom_blur(Ichannels[2].mul(p), r);
 
     // covariance of (I, p) in each local patch.
     cv::Mat cov_Ip_r = mean_Ip_r - mean_I_r.mul(mean_p);
@@ -179,10 +184,10 @@ cv::Mat guidedFilterColor::filterSingleChannel(const cv::Mat &p) const
 
     cv::Mat b = mean_p - a_r.mul(mean_I_r) - a_g.mul(mean_I_g) - a_b.mul(mean_I_b); // Eqn. (15) in the paper;
 
-    return (boxfilter(a_r, r).mul(Ichannels[0])
-          + boxfilter(a_g, r).mul(Ichannels[1])
-          + boxfilter(a_b, r).mul(Ichannels[2])
-          + boxfilter(b, r));  // Eqn. (16) in the paper;
+    return (custom_blur(a_r, r).mul(Ichannels[0])
+          + custom_blur(a_g, r).mul(Ichannels[1])
+          + custom_blur(a_b, r).mul(Ichannels[2])
+          + custom_blur(b, r));  // Eqn. (16) in the paper;
 }
 
 
